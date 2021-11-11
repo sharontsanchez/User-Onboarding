879bc4885704f708fc338754ea7a7d0f41f21628
Sharon Sanchez <sstharakan@gmail.com>
2021-11-10 22:03:44 -0600

    set up project

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './component/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>User-Onboarding Project</h2>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/component/Form.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+
+const Form = (props) => {
+
+    return(
+    /* Make your Form */
+        <div>
+            <h1>My Cool Form!</h1>
+        </div>
+    )
+};
+
+export default Form;
